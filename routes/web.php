9d5dfe7d8792b851553f91b7635ffeac08506a7e
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2019-03-14 10:16:34 +0200

    Add auth system

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/posts', 'PostsController@index')->name('posts.index');
 Route::get('/posts/create', 'PostsController@create')->name('posts.create');
 Route::post('/posts','PostsController@store')->name('posts.store');
 Route::get('/posts/{post}/edit','PostsController@edit')->name('posts.edit');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
