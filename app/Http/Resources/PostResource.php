bdf00c82947e123e251763e8ede4eeab0a902556
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2019-03-15 10:08:58 +0200

    Add transforming post model object

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,6 +14,11 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'created_at' => $this->created_at->toDateString()
+        ];
     }
 }
